9165f92e236e561530b96056b3177aee7e5a98d8
k-pylinskyi <k.pylinskyi@gmail.com>
2022-08-08 20:49:08 +0200

    Download Processor. Archive Extracter Added

--- Services/DownloadProcessor.py
+++ Services/DownloadProcessor.py
@@ -1,11 +1,13 @@
 from Services.DbService import *
 from Services.FtpConection import FtpConnection
+from Services.Extracter import Extracter
 
 class DownloadProcessor:
     def __init__(self):
         db = DbService()
         self.storage_path = 'D:\\Work\\MNK_PRICES\\DB_FILES'
         self.ftp_download_list = db.select('select_ftp_download.sql')
+        self.archive_files_list = db.select('select_archive_files.sql')
 
     def download(self):
         self.create_suplier_folder()
@@ -20,10 +22,21 @@ class DownloadProcessor:
             ftp = FtpConnection(ip, login, passw)
             local_file = os.path.join(self.storage_path, local, filename)
             ftp.download_file(remote, local_file)
+
+        self.extract_files()
+
     def create_suplier_folder(self):
         for row in self.ftp_download_list:
             local_folder = row[5]
             local_path = os.path.join(self.storage_path, local_folder)
             if not os.path.exists(local_path):
                 os.makedirs(local_path)
-                print('created {}'.format(local_folder))
+                print('folder created {}'.format(local_path))
+
+    def extract_files(self):
+        for row in self.archive_files_list:
+            archive_path = os.path.join(self.storage_path, row[1])
+            out_dir = os.path.join(self.storage_path, row[2])
+
+            Extracter.extract(archive_path, out_dir)
+
--- Services/Extracter.py
+++ Services/Extracter.py
@@ -1,9 +1,6 @@
 import patoolib
 
 class Extracter:
-    def __init__(self):
-        pass
-
     @staticmethod
-    def extract(self, archive_path, out_dir):
+    def extract(archive_path, out_dir):
         patoolib.extract_archive(archive_path, outdir=out_dir)
