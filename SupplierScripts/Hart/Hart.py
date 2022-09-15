e8d62f0a6bfc3568a4e756cc1a8c85ab69173594
k-pylinskyi <k.pylinskyi@gmail.com>
2022-09-15 13:41:48 +0200

    Hart

--- SupplierScripts/Hart/Hart.py
+++ SupplierScripts/Hart/Hart.py
@@ -1,10 +1,31 @@
 from SupplierScripts.DataFrameReader import DataFrameReader as dfreader
-import pandasql as pdsql
+from pandasql import sqldf
 
 class Hart:
     def __init__(self):
         directory = "./TemporaryStorage//Hart//files"
-        dfs_dict = dfreader.get_data_frames(directory)
+        self.dfs_dict = dfreader.get_data_frames(directory)
 
     def process(self):
-        pass
+        data = self.dfs_dict.get("hart_data")
+        data.columns = ['hart_part_number', 'tecdoc_number', 'part_number', 'part_name', 'unit_measure', 'price', 'deposit', 'oe_number', 'additional_numbers', 'ean_codes', 'origin']
+
+        quantity = self.dfs_dict.get("96285_Quantity")
+        quantity.columns = ['hart_part_number', 'qty', 'warehouse']
+
+        cn = self.dfs_dict.get("hart_cn")
+        cn.columns = ['hart_part_number', 'tariff_code', 'weight']
+
+        cross = self.dfs_dict.get("hart_cross")
+        cross.columns = ['hart_part_number', 'part_number', 'part_name', 'hart_part_number_cross', 'part_name_cross', 'supplier_cross']
+
+        deposit = self.dfs_dict.get("hart_deposit")
+        deposit.columns = []
+
+        price = self.dfs_dict.get("hart_price")
+
+        weight = self.dfs_dict.get("hart_weight")
+
+
+
+
