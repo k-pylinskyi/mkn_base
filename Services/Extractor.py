bfdc77d24c5e349cfcbdecbdab3f77733f22a953
garcondeau <garcondeau@icloud.com>
2022-09-15 16:04:36 +0200

    added basic archive extractor

--- Services/Extractor.py
+++ Services/Extractor.py
@@ -1,6 +1,8 @@
 import gzip
 import os
+import platform
 import shutil
+import subprocess
 import zipfile
 
 from utils.consts import CONSOLE_COLOR, ERRORS
@@ -12,31 +14,43 @@ class Extractor:
         old_filename_path = os.path.join(out_folder, old_filename)
         new_filename_path = os.path.join(out_folder, new_filename)
 
+        if platform.system() == 'Windows':
+            extractor = 'start 7z/windows/7z.exe'
+        elif platform.system() == 'Darwin':
+            extractor = '7z/darwin/7zz'
+        else:
+            extractor = '7z/linux/7zz'
+
         try:
-            if archive_path.lower().endswith('.zip'):
-
-                with zipfile.ZipFile(archive_path, 'r') as zip_ref:
-                    zip_ref.extractall(out_folder)
-
-                if os.path.exists(new_filename_path):
-                    os.remove(new_filename_path)
-
-                for root, dirs, files in os.walk(out_folder):
-                    for file in files:
-                        if file.startswith(old_filename):
-                            os.rename(old_filename_path, new_filename_path)
-                            print(f'File renamed {old_filename_path} to {new_filename_path}\n')
-
-                if os.path.exists(old_filename_path):
-                    os.remove(old_filename_path)
-
-                print(f'ZIP file extracted {new_filename_path}\n')
-
-            elif archive_path.lower().endswith('.gz'):
-                with gzip.open(archive_path, 'rb') as file_in:
-                    with open(new_filename_path, 'wb') as file_out:
-                        shutil.copyfileobj(file_in, file_out)
-                print(f'GZ file extracted {new_filename_path}\n')
-
+            subprocess.run([extractor, archive_path, "-o", out_folder])
         except Exception as ex:
             print(f'{CONSOLE_COLOR.ERROR}{ERRORS.FILE_ERROR} {new_filename_path} {ex}{CONSOLE_COLOR.NC}\n')
+        #
+        # try:
+        #     if archive_path.lower().endswith('.zip'):
+        #
+        #         with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+        #             zip_ref.extractall(out_folder)
+        #
+        #         if os.path.exists(new_filename_path):
+        #             os.remove(new_filename_path)
+        #
+        #         for root, dirs, files in os.walk(out_folder):
+        #             for file in files:
+        #                 if file.startswith(old_filename):
+        #                     os.rename(old_filename_path, new_filename_path)
+        #                     print(f'File renamed {old_filename_path} to {new_filename_path}\n')
+        #
+        #         if os.path.exists(old_filename_path):
+        #             os.remove(old_filename_path)
+        #
+        #         print(f'ZIP file extracted {new_filename_path}\n')
+        #
+        #     elif archive_path.lower().endswith('.gz'):
+        #         with gzip.open(archive_path, 'rb') as file_in:
+        #             with open(new_filename_path, 'wb') as file_out:
+        #                 shutil.copyfileobj(file_in, file_out)
+        #         print(f'GZ file extracted {new_filename_path}\n')
+        #
+        # except Exception as ex:
+        #     print(f'{CONSOLE_COLOR.ERROR}{ERRORS.FILE_ERROR} {new_filename_path} {ex}{CONSOLE_COLOR.NC}\n')
