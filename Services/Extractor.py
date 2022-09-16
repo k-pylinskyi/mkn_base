8ff7abccb101abcdb2d942ae8cf8de64812d60fb
garcondeau <garcondeau@icloud.com>
2022-09-16 12:00:34 +0200

    fixed file renaming

--- Services/Extractor.py
+++ Services/Extractor.py
@@ -32,17 +32,6 @@ class Extractor:
                     subprocess.run([extractor, "e", archive_path, f'-o{out_folder}', "-y"])
                 except Exception as ex:
                     print(f'{CONSOLE_COLOR.ERROR}{ERRORS.FILE_ERROR} {new_filename_path} {ex}{CONSOLE_COLOR.NC}\n')
-            if os.path.exists(new_filename_path):
-                os.remove(new_filename_path)
-
-            for root, dirs, files in os.walk(out_folder):
-                for file in files:
-                    if file.startswith(old_filename):
-                        os.rename(old_filename_path, new_filename_path)
-                        print(f'File renamed {old_filename_path} to {new_filename_path}\n')
-
-            if os.path.exists(old_filename_path):
-                os.remove(old_filename_path)
         elif platform.system() == 'Linux':
             extractor = '7z/linux/7zz'
             try:
@@ -52,3 +41,14 @@ class Extractor:
                 print(f'{CONSOLE_COLOR.ERROR}{ERRORS.FILE_ERROR} {new_filename_path} {ex}{CONSOLE_COLOR.NC}\n')
         else:
             print('Unsupported platform')
+        if os.path.exists(new_filename_path):
+            os.remove(new_filename_path)
+
+        for root, dirs, files in os.walk(out_folder):
+            for file in files:
+                if file.startswith(old_filename):
+                    os.rename(old_filename_path, new_filename_path)
+                    print(f'File renamed {old_filename_path} to {new_filename_path}\n')
+
+        if os.path.exists(old_filename_path):
+            os.remove(old_filename_path)
