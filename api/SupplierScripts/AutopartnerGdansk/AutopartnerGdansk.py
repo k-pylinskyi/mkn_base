76683c46f8803a020a510dca898852407f6b8af1
garcondeau <garcondeau@icloud.com>
2022-09-21 11:40:02 +0200

    added AutopartnerGdansk.py script

--- api/SupplierScripts/AutopartnerGdansk/AutopartnerGdansk.py
+++ api/SupplierScripts/AutopartnerGdansk/AutopartnerGdansk.py
@@ -1,52 +1,40 @@
 import pandas as pd
 from pandasql import sqldf
-import re
-
-from api.Services.Processors.DataFrameReader import DataFrameReader
 
 
 class AutopartnerGdansk:
+    pd.set_option('display.max_columns', 999)
+
     def __init__(self):
-        dictionary = './autopartner_gdansk_helper.csv'
-        location = '../../../TemporaryStorage/AUTO_PARTNER_GDANSK/files/autopartner_gdansk_data.csv'
-        self.data = pd.read_csv(location, sep=';', skiprows=1, decimal=',', low_memory=False, encoding_errors='ignore')
-        self.dictionary = pd.read_csv(dictionary, sep='\t', skiprows=1, decimal=',', low_memory=False,
-                                      encoding_errors='ignore')
+        self.data_columns = {0: 'manufacturer', 1: 'part_name', 2: 'part_art', 3: 'qty', 4: 'price', 6: 'part_number'}
+        self.dict_columns = {0: 'part_art', 1: 'part_number', 2: 'manufacturer', 3: 'part_name'}
+        location = './TemporaryStorage/AUTO_PARTNER_GDANSK/files/autopartner_gdansk_data.csv'
+        dictionary = './api/SupplierScripts/AutopartnerGdansk/autopartner_gdansk_helper.csv'
+
+        self.data = pd.read_csv(location, encoding_errors='ignore', sep=';', header=None, low_memory=False)
+        self.dict = pd.read_csv(dictionary, encoding_errors='ignore', sep='\t', header=None, skiprows=1)
 
     def process(self):
-        self.dictionary.columns = [
-            'supplier_part_number',
-            'part_number',
-            'manufacturer',
-            'part_name'
-        ]
-        self.data.columns = [
-            'supplier_part_number',
-            'part_name',
-            'supplier_manufacturer',
-            'qty',
-            'price',
-            '',
-            'part_number',
-            '',
-            '',
-        ]
+        self.data.drop(self.data.columns[[5, 7, 8]], axis=1, inplace=True)
+        self.data.rename(columns=self.data_columns, inplace=True)
+        self.data.set_index('part_number', inplace=True)
 
-        self.data['part_number'] = DataFrameReader.format_column(self.data['part_number'])
+        self.dict.rename(columns=self.dict_columns, inplace=True)
+        self.dict.set_index('part_number', inplace=True)
+        return [self.data, self.dict]
 
 
-        query = """
-        SELECT DISTINCT 7 AS supplier_id,
-        data.supplier_part_number AS supplier_part_number
+def process_autopartner_gdansk():
+    autopartner_gdansk = AutopartnerGdansk()
+    dataframes = autopartner_gdansk.process()
+    data = dataframes[0]
+    dict = dataframes[1]
+    query = '''
+        SELECT dict.part_number, dict.manufacturer, data.part_name, data.price,
+        REPLACE(REPLACE(data.qty, '-', '0'), '>', '') AS qty
         FROM data
-        """
-        # print(self.dictionary)
-        # print(self.data)
-        self.data.dropna(how='any', axis=1)
-        # nan_val = float("NaN")
-        # self.data.replace("test", nan_val, inplace=True)
-        print(self.data)
-
-
-autopartner_gdansk = AutopartnerGdansk()
-autopartner_gdansk.process()
+        INNER JOIN dict
+        ON dict.part_art = data.part_art
+        WHERE qty NOT LIKE '0'
+    '''
+    return sqldf(query)
