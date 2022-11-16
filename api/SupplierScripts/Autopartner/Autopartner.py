2b9b0c4d0bd99e31068581e6f9c94dac3f85a506
Tatre <polinap5411@hmail.com>
2022-11-16 11:51:00 +0100

    fix elit and autopartner

--- api/SupplierScripts/Autopartner/Autopartner.py
+++ api/SupplierScripts/Autopartner/Autopartner.py
@@ -38,7 +38,7 @@ class Autopartner:
                              6: 'currency', 7: 'weight', 9: 'bar_code', 10: 'supplier_part_number', 11: 'part_description',
                              12: 'qty1', 15: 'qty2', 16: 'qty3', 17: 'manufacturer_code'}
 
-        self.data = pd.read_csv(data, encoding_errors='ignore', sep=';', on_bad_lines=False, header=None, low_memory=False)
+        self.data = pd.read_csv(data, encoding_errors='ignore', sep=';', error_bad_lines=False, header=None, low_memory=False)
 
     def process(self):
         self.data.drop(self.data.columns[[5, 8, 13, 14]], axis=1, inplace=True)
