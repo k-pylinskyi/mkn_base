7fe6af160aa85b4a8ec0941e4a306c50e9b88330
Tatre <polinap5411@hmail.com>
2022-12-21 17:24:59 +0100

    update DataFrameReader

--- api/Services/Processors/DataFrameReader.py
+++ api/Services/Processors/DataFrameReader.py
@@ -25,6 +25,15 @@ class DataFrameReader:
         dataframe.to_sql(table_name, connection, if_exists='replace', index=False)
         DataFrameReader.supplier_to_ftp(table_name)
 
+    def dataframe_to_db_multi(table_name, dataframe, folder):
+        context = DbContext()
+        connection = context.db
+        dataframe['part_number'] = format_column(dataframe['part_number'])
+        dataframe['manufacturer'] = dataframe['manufacturer'].str.upper()
+        dataframe['timestamp'] = datetime.datetime.today().strftime('%d-%m-%Y')
+        dataframe.to_sql(table_name, connection, if_exists='replace', index=False)
+        DataFrameReader.supplier_to_ftp_multi(table_name, folder)
+
     @staticmethod
     def supplier_to_ftp(supplier):
         db = DbService()
@@ -32,6 +41,13 @@ class DataFrameReader:
         file = db.get_table_csv(supplier)
         ftp = FtpConnection('138.201.56.185', 'ph6802', 'z7lIh8iv10pLRt')
         ftp.upload_file(file, 'maxi_export', supplier)
+
+    def supplier_to_ftp_multi(supplier, folder):
+        db = DbService()
+        print('Exporting {} to csv'.format(supplier))
+        file = db.get_table_csv(supplier)
+        ftp = FtpConnection('138.201.56.185', 'ph6802', 'z7lIh8iv10pLRt')
+        ftp.upload_file(file, 'maxi_export/' + str(folder), supplier)
 
     @staticmethod
     def dataframe_to_db_big(table_name, dataframe):
